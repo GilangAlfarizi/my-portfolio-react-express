1e47321bd288cbebacc94b1a3ef7c354daa9f76d
GilangAlfarizi <alfarizigilang666@gmail.com>
2024-11-18 06:56:27 +0700

    fix: config redis url to upstash

--- server/config/redisConfig.js
+++ server/config/redisConfig.js
@@ -1,7 +1,13 @@
 const Redis = require("redis");
 
 // Create Redis client
-const redisClient = Redis.createClient();
+const redisClient = Redis.createClient({
+	url: process.env.REDIS_URL,
+	socket: {
+		tls: true,
+		rejectUnauthorized: false,
+	},
+});
 
 redisClient.on("error", (err) => console.error("Redis Client Error", err));
 
